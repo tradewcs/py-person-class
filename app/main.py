d2c8080873c0776450af50645d9d3e54a0b8a4b7
Mike <tradewcs@gmail.com>
2025-09-25 15:10:24 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -12,9 +12,9 @@ def create_person_list(people: list) -> list:
 
     for person_dict in people:
         person = Person.people[person_dict["name"]]
-        if person_dict.get('wife') is not None:
+        if person_dict.get("wife") is not None:
             person.wife = Person.people[person_dict["wife"]]
-        if person_dict.get('husband') is not None:
+        if person_dict.get("husband") is not None:
             person.husband = Person.people[person_dict["husband"]]
 
     return person_list
