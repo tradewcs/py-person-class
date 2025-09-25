64c311480e63f98945f7a803a005bee2fd3e9989
Mike <tradewcs@gmail.com>
2025-09-25 15:12:18 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = [Person(person_dict["name"], person_dict["age"]) for person_dict in people]
+    person_list = [
+        Person(person_dict["name"], person_dict["age"])
+        for person_dict in people
+    ]
 
     for person_dict in people:
         person = Person.people[person_dict["name"]]
